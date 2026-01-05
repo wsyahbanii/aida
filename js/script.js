bbd9812720337d8739edfc3f39be2eb562d16643
Wahyu Syahbani <wahyuusyahbanii@gmail.com>
2026-01-06 03:35:35 +0700

    Add new stories and update display logic

--- js/script.js
+++ js/script.js
@@ -6,68 +6,15 @@ const myStories = [
     },
     {
         date: "31 Desember 2025 (Menjelang Tahun Baru)",
-        text: `Aku mau jawab surat kamu yg kamu tulis dibuku, jujur aku bener-bener tersentuh sama 
-        kalimat kamu. Aku mau ucapin terimakasih banyak sama kamu atas semua yang kamu beri,
-        ntah dari segi apapun itu aku ucapin terimakasih banyak yah sayang. Gatau nanti aku bakal
-        sampai rumah atau engga. Aku harap kamu selalu diberi kesehatan, diberi kebahagiaan. Aku 
-        bener-bener sayang sama kamu, aku bahagia banget bisa ketemu kamu aida, bisa mengenalmu. 
-        Dan aku minta maaf buat kesan pertama kita ketemu. aku bener-bener kagok kalau soal 
-        ngobrol sama perempuan, aku minta maaf gabisa ngasih yang terbaik buat kamu. Ntah itu 
-        dalam memperlakukanmu ketika ketemu, gaya bicaraku yang mungkin bisa bikin kamu sakit 
-        hati. Aku bener- benar minta maaf yah sayang. Aku harap kamu bisa maafin aku. Aku janji 
-        aku bakal berusaha jadi yang terbaik buat kamu, aku bakal berusaha buat bikin kamu bahagia. 
-        Aku sayang kamu aida.
-        \nDan aku juga mau berterimakaih banyakk udh mau nerima aku yg apa adanya ini makasih 
-        bangettt. atas kamu yg telah menerima baik buruknya aku, masalalu aku, aku bener-bener 
-        ganyangka kalau bisa ketemu perempuan yg bakal bisa nerima aku APA ADANYA. Aku gatau 
-        harus gimana buat bales kebaikan kamu, aku harap kita bisa jalanin ini semua bareng-bareng 
-        yah sayanggg. Aku mau kamu jadi menantu ibu aku nantinya. Pasti dia seneng banget bisa 
-        punya menantu kaya kamu. Semmoga aja si apa yg abang ojek tadi bilang bener-bener bisa 
-        terwujud yahhh istri akuhhhhh :)
-        \n Jujur si sedih banget aku buat pisah sama kamu malem ini, dengan waktu yg amat singkat ini. 
-        Cuma mungkin kamu ngiranya aku biasa aja. Tapi hati kecil aku kya bener-bener sedih buat 
-        pisah sama kamu. "Jl. Veteran, Serangm Banten, ONIX Coffee" menjadi titik perpisahan kita 
-        dipenghujung tahun, "Ramayana Mall Serang Banten" menjadi titik awal pertemuan kita, 
-        "BK Studio Photobox" menjadi titik dimana aku bener-bener merasa kehadiranku berarti,
-        "MOMOYO Ice Cream" menjadi titik dimana kamu bisa membawa aku ke masa kecil lagi. 
-        \nSelamat Tahun Baru Aida Sayang. Semoga tahun ini menjadi takun kita untuk selamanya. 
-        Semoga hubungan kita kali ini bener-bener bjadi hubungan yg kita inginkan, menjadi hubungan
-        yang Allah restui, menjadi hubungan yang bisa meneruskan ke generasi kita selanjutnya. Dan
-        kelak semoga bisa menjadi hubungan yang sakinah, mawaddah, warohmah.
-        \nTolong ajari aku menjadi pria yang kamu inginkan, menjadi pria yang kamu idamkan, dan 
-        aku minta tolong buat dekatkan aku ke Tuhanmu lagi. Aku pengen bisa jadi imam kamu, bisa 
-        jadi imam buat anak-anak mu kelak, jadi ayah dari anak-anak mu (maaf kejauhan hehe). Tapi 
-        kan yang namanya hubungan udah pasti arahnya kesana kan? buat menjadi keluarga kecil yang
-        bahagia.
-        \nSemoga di tahun 2026 ini kita masih bisa dipertemukan lago, masih bisa main lagi, ntah
-        dimanapun itu. Jadikan dimanapun tempat itu kita bertemu menjadi titik yang berarti buat
-        kita berdua. Kalaupun bisa diseluruh Indonesia bahkan diseluruh dunia. Aku pengen banget.
-        \nAku harap juga kamu bisa datang ke tempatku kelak "di Solo" tempat yang katanya 
-        by reset/data tempat ternyaman No.1 di Indonesia dimana kita bisa jadikan tempat itu 
-        menjadi tempat yang selalu kamu rindukan, tempat yang selalu kamu ingat, tempat yang 
-        selalu kamu mau datengin lagi.`
+        text: `Aku mau jawab surat kamu yg kamu tulis dibuku, jujur aku bener-bener tersentuh sama kalimat kamu. Aku mau ucapin terimakasih banyak sama kamu atas semua yang kamu beri, ntah dari segi apapun itu aku ucapin terimakasih banyak yah sayang. Gatau nanti aku bakal sampai rumah atau engga. Aku harap kamu selalu diberi kesehatan, diberi kebahagiaan. Aku bener-bener sayang sama kamu, aku bahagia banget bisa ketemu kamu aida, bisa mengenalmu. Dan aku minta maaf buat kesan pertama kita ketemu. aku bener-bener kagok kalau soal ngobrol sama perempuan, aku minta maaf gabisa ngasih yang terbaik buat kamu. Ntah itu dalam memperlakukanmu ketika ketemu, gaya bicaraku yang mungkin bisa bikin kamu sakit hati. Aku bener- benar minta maaf yah sayang. Aku harap kamu bisa maafin aku. Aku janji aku bakal berusaha jadi yang terbaik buat kamu, aku bakal berusaha buat bikin kamu bahagia. Aku sayang kamu aida. \nDan aku juga mau berterimakaih banyakk udh mau nerima aku yg apa adanya ini makasih bangettt. atas kamu yg telah menerima baik buruknya aku, masalalu aku, aku bener-bener ganyangka kalau bisa ketemu perempuan yg bakal bisa nerima aku APA ADANYA. Aku gatau harus gimana buat bales kebaikan kamu, aku harap kita bisa jalanin ini semua bareng-bareng yah sayanggg. Aku mau kamu jadi menantu ibu aku nantinya. Pasti dia seneng banget bisa punya menantu kaya kamu. Semmoga aja si apa yg abang ojek tadi bilang bener-bener bisa terwujud yahhh istri akuhhhhh :) \n Jujur si sedih banget aku buat pisah sama kamu malem ini, dengan waktu yg amat singkat ini. Cuma mungkin kamu ngiranya aku biasa aja. Tapi hati kecil aku kya bener-bener sedih buat pisah sama kamu. "Jl. Veteran, Serangm Banten, ONIX Coffee" menjadi titik perpisahan kita dipenghujung tahun, "Ramayana Mall Serang Banten" menjadi titik awal pertemuan kita, "BK Studio Photobox" menjadi titik dimana aku bener-bener merasa kehadiranku berarti, "MOMOYO Ice Cream" menjadi titik dimana kamu bisa membawa aku ke masa kecil lagi. \nSelamat Tahun Baru Aida Sayang. Semoga tahun ini menjadi takun kita untuk selamanya. Semoga hubungan kita kali ini bener-bener bjadi hubungan yg kita inginkan, menjadi hubungan yang Allah restui, menjadi hubungan yang bisa meneruskan ke generasi kita selanjutnya. Dan kelak semoga bisa menjadi hubungan yang sakinah, mawaddah, warohmah. \nTolong ajari aku menjadi pria yang kamu inginkan, menjadi pria yang kamu idamkan, dan aku minta tolong buat dekatkan aku ke Tuhanmu lagi. Aku pengen bisa jadi imam kamu, bisa jadi imam buat anak-anak mu kelak, jadi ayah dari anak-anak mu (maaf kejauhan hehe). Tapi kan yang namanya hubungan udah pasti arahnya kesana kan? buat menjadi keluarga kecil yang bahagia. \nSemoga di tahun 2026 ini kita masih bisa dipertemukan lago, masih bisa main lagi, ntah dimanapun itu. Jadikan dimanapun tempat itu kita bertemu menjadi titik yang berarti buat kita berdua. Kalaupun bisa diseluruh Indonesia bahkan diseluruh dunia. Aku pengen banget. \nAku harap juga kamu bisa datang ke tempatku kelak "di Solo" tempat yang katanya by reset/data tempat ternyaman No.1 di Indonesia dimana kita bisa jadikan tempat itu menjadi tempat yang selalu kamu rindukan, tempat yang selalu kamu ingat, tempat yang selalu kamu mau datengin lagi.`
     },
     {
         date: "1 Januari 2026 00.00 WIB???",
-        text: `Bunyi petasan udah meronta-ronta, apakah ini sudah 2026 aida?? HP aku lagi 
-        dicharge, jadi gatau. Ini juga aku masih di kota Serang, masih di Indomart. Dan 
-        sekali lagi aku ucapkan Selamat tahun batu Sayang hihii. WOPYUUU WOPYUUU. Walaupun
-        kamu bukan jadi orang yang pertama yg aku tatap di tahun baru ini. Tapi kamu jadi orang
-        satu-satunya yang ada di hati aku di awal tahun 2026 ini. Aku harap kamu akan nyaman
-        disana (hati aku) untuk tetap tinggal sampai kapanpun itu. Dan kalaupun kamu kesepian
-        disana, suatu saat nanti akan ada yang menemanimu di hatiku. Siapa lagi kalau bukan
-        anak-anak mu kelak hehe. pastinya nanti aku juga bakal menyayangi mu dan anak-anakmu 
-        bukan? Anak ku juga si hihi masa anak kamu doang.
-        \nMasochism (Masokisme) kini dia telah pergi sejak datangnya kehadiranmu, mereka jadi takut
-        untuk tinggal diotak maupun jiwaku karenamu. Dalam artian aku sekarang mulai nyaman akan 
-        hal rasa bahagisa. Kebahagiaan tak lagi asing bagiku, tak lagi ada pernyataan "Kebahagiaan
-        itu dangkal" itu semua berkat kehadiranmu aida. Atas kehadiranmu kebahagiaan menjadi candu 
-        rasanya, menjadi hal yang selalu aku ingin rasakan, menjadi hal yang tak pernah aku ingin 
-        berhenti untuk merasakan hal itu. AKU BAHAGIA LAGII KARENAMU "AIDA KEKASIHKU".
-        \nKita mungkin bisa dibilang pacaran, bisa dibilang pasangan, bisa dibilang sedang melakukan
-        komitmet yang kita jalani dan kita sepakati untuk menjadi 2 orang yang akan selalu mencintai
-        sampai semuanya diambil alih lagi. "Dan btw jujur ini pertama kalinya aku jalan sama 
-        perempuan tapi bisa menyempatkan untuk shalat"`
+        text: `Bunyi petasan udah meronta-ronta, apakah ini sudah 2026 aida?? HP aku lagi dicharge, jadi gatau. Ini juga aku masih di kota Serang, masih di Indomart. Dan sekali lagi aku ucapkan Selamat tahun batu Sayang hihii. WOPYUUU WOPYUUU. Walaupunkamu bukan jadi orang yang pertama yg aku tatap di tahun baru ini. Tapi kamu jadi orangsatu-satunya yang ada di hati aku di awal tahun 2026 ini. Aku harap kamu akan nyaman disana (hati aku) untuk tetap tinggal sampai kapanpun itu. Dan kalaupun kamu kesepian disana, suatu saat nanti akan ada yang menemanimu di hatiku. Siapa lagi kalau bukan anak-anak mu kelak hehe. pastinya nanti aku juga bakal menyayangi mu dan anak-anakmu bukan? Anak ku juga si hihi masa anak kamu doang. \nMasochism (Masokisme) kini dia telah pergi sejak datangnya kehadiranmu, mereka jadi takut untuk tinggal diotak maupun jiwaku karenamu. Dalam artian aku sekarang mulai nyaman akan hal rasa bahagisa. Kebahagiaan tak lagi asing bagiku, tak lagi ada pernyataan "Kebahagiaan itu dangkal" itu semua berkat kehadiranmu aida. Atas kehadiranmu kebahagiaan menjadi candu rasanya, menjadi hal yang selalu aku ingin rasakan, menjadi hal yang tak pernah aku ingin berhenti untuk merasakan hal itu. AKU BAHAGIA LAGII KARENAMU "AIDA KEKASIHKU". \nKita mungkin bisa dibilang pacaran, bisa dibilang pasangan, bisa dibilang sedang melakukan komitmet yang kita jalani dan kita sepakati untuk menjadi 2 orang yang akan selalu mencintai sampai semuanya diambil alih lagi. "Dan btw jujur ini pertama kalinya aku jalan sama perempuan tapi bisa menyempatkan untuk shalat"`
+    },    
+    {
+        date: "5 Januari 2026 (Day 1)",
+        text: "Haii sayangkuuuh... idk why it feels like, im happy but it was flat tho. Beda banget ketika aku ada kamu, aku tdi bangun jam 10 dan sadar ahaha klo aku bangun bukan karena kamu lagii, aku bangun trs bilang ke bang onile kalo mau masuk tapi gajadi krna tbtb ngerasa masih gaenak badan, dan tidur lagi sampe jam 3 itu semua aku ga ngerasa sedih ataupun bahagia, kea flat aja gtu. Dan tiba tiba @daeeeyz.zhr was calling me, bener bener kaget seneng ayyyy :(. Kamu jaga kesehatan yah disana sayangkuuuu. Sampai jumpa lagi dilain waktu sayangkuuuu. I Love You Aida Kekasihkuuu <3"
     },
 ];
 
@@ -113,4 +60,5 @@ function jumpToStory(val) {
 window.onload = () => {
     populateSelect();
     updateDisplay();
+
 };
