51a1cd9b9990b36da1b06d4d44e758af9b34473d
Wahyu Syahbani <wahyuusyahbanii@gmail.com>
2026-01-13 01:57:10 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ const myStories = [
     },
     {
         date: "13 Januari 2026 3.43 (Day 9)",
-        text: `I'm tired......but I won't give up.`
+        text: `I'm tired........but I won't give up.`
     }
 ];
 
@@ -73,6 +73,7 @@ window.onload = () => {
     populateSelect();
     updateDisplay();
 };
+
 
 
 
